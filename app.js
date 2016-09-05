4e528acf604354a23599c781e4b4ae8808b22a89
Adrorriver <Adrorriver@gmail.com>
2016-09-05 18:02:19 +0300

    a

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello world');
+app.use(express.static('public'));
+
+app.post('/', function (req, res) {
+    res.send('post Hello world');
 })    
 
 app.listen(3000, function () {
