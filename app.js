0f5a433b503c7d50feb8bde64bc7aa7443365383
Adrorriver <Adrorriver@gmail.com>
2016-09-06 00:20:04 +0300

    go

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.send('get Hello world');
-}) 
+}); 
 
 app.post('/', function (req, res) {
     res.send('post Hello world');
-})    
+});    
 
 app.listen(3000, function () {
     console.log('Example app: ')
-}) 
+});
